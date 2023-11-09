b9e5cb111cd9406035df56c7980d7d9b2ecc1ac6
LucaskDev <142241532+LucaskDev@users.noreply.github.com>
2023-11-09 12:54:23 -0300

    Add files via upload

---
+++ Lucas/src/Main.java
@@ -0,0 +1,148 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    //String planeta = "Plutão";
+    //System.out.println(planeta);
+
+    //    Scanner entrada = new Scanner(System.in);
+        //System.in atribui um valor para o usuário adicionar, ele pega uma informação
+    //    System.out.println("Digite seu nome: ");
+    //    String nome = entrada.next();
+        //nextLine Transforma um valor em Texto (String)
+                //00102025020
+    //    System.out.println("Olá, " + nome);
+
+    //    Scanner entrada = new Scanner(System.in);
+
+    //    System.out.println("Informe o seu nome: ");
+    //    String nome = entrada.nextLine();
+
+    //    System.out.println("Informe sua idade: ");
+    //    String idade = entrada.nextLine();
+
+    //    System.out.println("Olá " + nome + ", sua idade é " + idade);
+
+
+          // Retangulo: A = Base.Altura
+
+    //    Scanner entrada = new Scanner(System.in);
+
+    //    System.out.println("Digite a altura");
+    //    int altura = entrada.nextInt();
+
+    //    System.out.println("Digite a base");
+    //    int base = entrada.nextInt();
+
+    //    int resultado = altura*base;
+
+    //    System.out.println("A área do retangulo é: " + resultado);
+
+
+          //Scanner entrada = new Scanner(System.in);
+
+          //System.out.println("Digite o primeiro lado");
+          //int ladoA = entrada.nextInt();
+
+          //System.out.println("Digite o segundo lado");
+          //int ladoB = entrada.nextInt();
+
+          //int resultado = ladoA*ladoB;
+
+          //System.out.println(resultado + "²");
+
+    //    Scanner entrada = new Scanner(System.in);
+
+    //    System.out.println("Digite a diagonal maior");
+    //    int ladoA = entrada.nextInt();
+
+    //    System.out.println("Digite a diagonal menor");
+    //    int ladoB = entrada.nextInt();
+
+    //    int resultado = ladoA*ladoB/2;
+
+    //    System.out.println(resultado);
+
+
+    //    Scanner entrada = new Scanner(System.in);
+
+    //    System.out.println("Informe a base maior");
+    //    int baseMaior = entrada.nextInt();
+
+    //    System.out.println("Informe a base menor");
+    //    int baseMenor = entrada.nextInt();
+
+    //    System.out.println("Informe o valor da altura (h)");
+    //    int h = entrada.nextInt();
+
+    //    int resultado = baseMaior + baseMenor * h /2;
+
+    //    System.out.println("O tamanho do trapézio é: " + resultado);
+
+        //System.out.println();
+
+    //    Scanner entrada = new Scanner(System.in);
+
+    //    System.out.println("Informe o valor da base");
+    //    int base = entrada.nextInt();
+
+    //    System.out.println("Informe o valor da altura");
+    //    int altura = entrada.nextInt();
+    //    int resultado = base * altura;
+
+    //    System.out.println("O tamanho do paralelograma é: " + resultado);
+
+
+    //    Scanner entrada = new Scanner(System.in);
+    //    System.out.println("Informe o valor do raio.");
+
+    //    int raio = entrada.nextInt();
+    //    double pi = Math.PI;
+
+    //    double resultado = pi * raio;
+
+    //    System.out.println("O círculo tem a área de: " + resultado);
+
+
+
+    //    Scanner entrada = new Scanner(System.in);
+    //    System.out.println("Informe um valor: ");
+
+    //    int valor = entrada.nextInt();
+
+    //    if (valor > 0){
+    //        System.out.println("O valor é positivo");
+    //    } else if (valor == 0) {
+    //        System.out.println("O valor é neutro");
+    //    }
+    //           else {
+    //        System.out.println("O valor é negativo.");
+    //    }
+
+     //   System.out.println();
+
+
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Informe o valor 1: ");
+        int valor1 = entrada.nextInt();
+
+        System.out.println("Informe o valor 2: ");
+        int valor2 = entrada.nextInt();
+
+        System.out.println("Informe o valor 3: ");
+        int valor3 = entrada.nextInt();
+
+
+
+
+
+
+    }
+}
